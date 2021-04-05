3e10246476aed48c2fc6d40294811372044aae89
Jeet Gandhi <jeet.gandhi@zuhlke.com>
2021-04-05 14:53:58 +0800

    Initial Commit

---
+++ Distance Tracker/Distance_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Distance_TrackerApp.swift
+//  Distance Tracker
+//
+//  Created by Jeet Gandhi on 5/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Distance_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
